8948f6f0c5819f13d9e7259ea17a73ddc23773c4
Stafferson <71585948+Stafferson@users.noreply.github.com>
2021-12-09 08:45:18 +0600

    Gay

--- app/src/main/java/com/example/first_welcome_screen/main_screen_not_welcome.java
+++ app/src/main/java/com/example/first_welcome_screen/main_screen_not_welcome.java
@@ -3,8 +3,11 @@ package com.example.first_welcome_screen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class main_screen_not_welcome extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
