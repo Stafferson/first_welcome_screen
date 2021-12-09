dac52aff5559926ea8e77499f0f11658b1596887
Stafferson <71585948+Stafferson@users.noreply.github.com>
2021-12-09 09:50:42 +0600

    not ready yet, just to save

--- app/src/main/java/com/example/first_welcome_screen/main_screen_not_welcome.java
+++ app/src/main/java/com/example/first_welcome_screen/main_screen_not_welcome.java
@@ -15,7 +15,8 @@ public class main_screen_not_welcome extends AppCompatActivity {
 
     ListView lv;
     EditText et;
-    ArrayList<String> classes = new ArrayList<String>();
+
+    String[] arr_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +33,31 @@ public class main_screen_not_welcome extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                System.out.println(s);
+                System.out.println("gay");
+                System.out.println(start);
+                System.out.println(before);
+                System.out.println(count);
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                System.out.println("?????");
+                System.out.println(s);
             }
         });
 
-        String[] arr_str = {"A", "B", "C", "D", "E", "F", "K", "L", "XZ", "A", "B", "C", "D", "E", "F", "K", "L", "XZ", "A", "B", "C", "D", "E", "F", "K", "L", "XZ", "A", "B", "C", "D", "E", "F", "K", "L", "XZ", "A", "B", "C", "D", "E", "F", "K", "L", "XZ", };
+        arr_str = new String[] {"7 A", "7 B", "7 C", "7 D", "7 E", "7 F", "8 A", "8 B", "8 C", "8 D", "8 E", "8 F", "8 K", "8 L", "9 A", "9 B", "9 C", "9 D", "9 E", "9 F", "9 K", "9 L", "10 A", "10 B", "10 C", "10 D", "10 E", "10 F", "10 K", "10 XZ", "11 A", "11 B", "11 C", "11 D", "11 E", "11 F"};
 
+    }
+
+    public ArrayList<String> is_in_arr(String [] arr, String request) {
+        ArrayList<String> arr_ans = new ArrayList<String>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].contains(request)){
+                arr_ans.add(arr[i]);
+            }
+        }
+        return arr_ans;
     }
 }
